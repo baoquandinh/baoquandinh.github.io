8a2f57c90fdf6777210b4efbf0ad31ef26c3d031
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-07-19 23:44:47 -0700

    Added portfolio project to main repository

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// const startTime = performance.now();
-
-let game = new Game();
-game.start();
-// const endTime = performance.now();
-// console.log(endTime - startTime + ` milliseconds`);
-
-const restartButton = document.querySelector('.restart');
-restartButton.addEventListener('click', game.restart);
