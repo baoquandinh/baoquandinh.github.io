abf6924163c9b7a39aca6f8711525106a8d94571
Baoquan Dinh <baoquanxdinh@gmail.com>
2021-05-14 00:32:00 -0700

    Removed navbar and set transitions to different section

--- baoquandinhportfolio/src/App.tsx
+++ baoquandinhportfolio/src/App.tsx
@@ -1,25 +1,15 @@
 import './App.css';
 import { AboutMePage, SkillsPage, ProjectsPage, ContactMePage } from './pages/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Route } from 'react-router-dom';
-import { PortfolioNavbar } from './components/NavBar/PortfolioNavbar';
 import 'aos/dist/aos.css';
 import AOS from 'aos'
 
 function App() {
   AOS.init()
   return (
-    <div className="App" style={{ height: '100vh' }}>
-      {/* <PortfolioNavbar />
-      <Switch>
-        <Route exact path='/' component={AboutMePage} />
-        <Route path='/skills' component={SkillsPage} />
-        <Route path='/projects' component={ProjectsPage} />
-        <Route path='/contact' component={ContactMePage} />
-      </Switch> */}
-      {/* <div data-aos="fade-in" data-aos-duration="2000"> */}
+    <div className="App">
       <AboutMePage />
-      {/* </div> */}
+      <ProjectsPage />
       <ContactMePage />
     </div>
   );
