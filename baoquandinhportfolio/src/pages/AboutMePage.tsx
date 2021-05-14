eb8901bb4c1c426362c75552271f69a647c1f60a
Baoquan Dinh <baoquanxdinh@gmail.com>
2021-05-14 00:32:12 -0700

    CSS styling

--- baoquandinhportfolio/src/pages/AboutMePage.tsx
+++ baoquandinhportfolio/src/pages/AboutMePage.tsx
@@ -3,15 +3,12 @@ import { Container, Row, Col, Image } from 'react-bootstrap'
 import { AboutMeDescription } from '../components/AboutMe/AboutMeDescription'
 
 export const AboutMePage = () => {
-    return <Container style={{height: '100vh'}}>
-        <Row className="h-100">
-            {/* <Col data-aos="fade-right" data-aos-duration='2000' style={{marginTop: '15%'}} md={4}>
-                <Image style={{ height: '250px', width: '240px' }} src={`${process.env.PUBLIC_URL}/images/test-profile.jpg`} roundedCircle />
-            </Col> */}
-            <Col md={12} style={{ paddingLeft: '40px', marginTop: '15%' }}>
+    return <Container id="about-me-container">
+        <Row className="align-items-center" style={{height: "100vh"}}>
+            <Col md={12}>
                 <AboutMeDescription />
             </Col>
-            <Col data-aos="fade-in" data-aos-duration="2000" data-aos-delay="1500" md={12}><p>Scroll Down</p></Col>
+            <Col style={{cursor:"pointer"}} data-aos="fade-in" data-aos-duration="2000" data-aos-delay="2000" data-aos-anchor-placement="contact-me-container" md={12}><p>Scroll Down</p></Col>
         </Row>
     </Container>
 }
