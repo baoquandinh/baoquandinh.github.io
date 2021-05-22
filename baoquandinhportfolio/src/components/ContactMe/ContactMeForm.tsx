44172543efc2e9658faefdd70f9156160f07ec6e
Baoquan Dinh <baoquanxdinh@gmail.com>
2021-05-22 11:39:42 -0700

    Saving progress on input validation for ContactaForm

--- baoquandinhportfolio/src/components/ContactMe/ContactMeForm.tsx
+++ baoquandinhportfolio/src/components/ContactMe/ContactMeForm.tsx
@@ -1,7 +1,7 @@
 import { BaseSyntheticEvent, Component, Fragment, SyntheticEvent } from 'react';
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import emailjs from 'emailjs-com';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Grid, FormControl, InputLabel, FormHelperText, Input, Button } from "@material-ui/core";
 
 
 interface IProp { }
@@ -9,7 +9,7 @@ interface IState {
     email: string;
     message: string;
     fullName: string;
-    formValidated: boolean;
+    fullNameError: boolean;
 }
 
 export class ContactMeForm extends Component<IProp, IState> {
@@ -19,10 +19,19 @@ export class ContactMeForm extends Component<IProp, IState> {
             fullName: '',
             email: '',
             message: '',
-            formValidated: false
+            fullNameError: false
         }
         this.sendEmail = this.sendEmail.bind(this)
         this.clearFormData = this.clearFormData.bind(this)
+        this.validateInput = this.validateInput.bind(this)
+    }
+
+    validateInput = (key: string) => {
+       console.log(key)
+       this.setState((state) => ({
+           ...state,
+           [`${key}Error`]: state.fullName == '' ? true : false
+       }))
     }
 
     clearFormData = () => {
@@ -30,30 +39,28 @@ export class ContactMeForm extends Component<IProp, IState> {
             fullName: '',
             email: '',
             message: '',
-            formValidated: false
+            fullNameError: false
         })
     }
 
     sendEmail = (e: SyntheticEvent) => {
         e.preventDefault();
         e.stopPropagation();
-        const { fullName, email, message } = this.state
-        const form = e.currentTarget as HTMLSelectElement
-        if (form.checkValidity() === true) {
-            const templateParams = {
-                name: fullName,
-                message: message,
-                email: email
-            }
-            emailjs.send('service_86dgq1p', 'template_7axkvtc', templateParams, 'user_UfC3xQimf9UCPJKwL75dC').then((response) => {
-                console.log(response)
-                this.clearFormData()
-            }, (error) => {
-                console.log(error)
-            })
+        const { fullName, email, message, fullNameError } = this.state
+        if (fullName == '' || email == '' || message == '') {
+            return;
         }
-        this.setState({ formValidated: true })
-
+        const templateParams = {
+            name: fullName,
+            message: message,
+            email: email
+        }
+        emailjs.send('service_86dgq1p', 'template_7axkvtc', templateParams, 'user_UfC3xQimf9UCPJKwL75dC').then((response) => {
+            console.log(response)
+            this.clearFormData()
+        }, (error) => {
+            console.log(error)
+        })
     }
     handleChange = (e: BaseSyntheticEvent) => {
         this.setState(state => ({
@@ -63,33 +70,29 @@ export class ContactMeForm extends Component<IProp, IState> {
     }
 
     render() {
-        return <Fragment>
-            <Form noValidate validated={this.state.formValidated} onSubmit={this.sendEmail}>
-                <Form.Row>
-                    <Form.Group className="col-md-6">
-                        <Form.Label className="float-left">Name</Form.Label>
-                        <Form.Control required name='fullName' placeholder="Enter Name" value={this.state.fullName} onChange={this.handleChange}></Form.Control>
-                        <Form.Control.Feedback type="valid">Nice first name</Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">You are missing your first name</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group className="col-md-6">
-                        <Form.Label className="float-left">Email</Form.Label>
-                        <Form.Control required name='email' type="email" placeholder="Enter Email Address" value={this.state.email} onChange={this.handleChange}></Form.Control>
-                        <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">Invalid Email Address</Form.Control.Feedback>
-                        <Form.Text className="text-muted">I'll never share your email with anyone so don't worry</Form.Text>
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                    <Form.Group className="col-md-12" controlId="formMessage">
-                        <Form.Label className="float-left">Message</Form.Label>
-                        <Form.Control required name='message' as="textarea" rows={4} placeholder="Looking forward to reading your message!" value={this.state.message} onChange={this.handleChange}></Form.Control>
-                        <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">Please leave a message</Form.Control.Feedback>
-                    </Form.Group>
-                </Form.Row>
-                <Button style={{ borderRadius: '40px' }} type="submit" variant="primary" size="lg" >SEND</Button>
-            </Form>
-        </Fragment>
+        return <Grid container spacing={1} justify="center" data-aos="fade-in" data-aos-duration="1000">
+            <Grid item xs={12} style={{ paddingTop: "40px" }}>
+                <FormControl id="full-name-input-form" error={this.state.fullNameError} variant="filled" required style={{ width: "75%" }}>
+                    <InputLabel htmlFor="full-name-input">Name</InputLabel>
+                    <Input id="full-name-input" name="fullName" aria-describedby="full-name-input-field" value={this.state.fullName} onChange={this.handleChange} onBlur={() => this.validateInput('fullName')}/>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl variant="filled" required style={{ width: "75%" }}>
+                    <InputLabel htmlFor="email-input">Email</InputLabel>
+                    <Input id="email-input" name="email" aria-describedby="email-address-input-field-helper-text" value={this.state.email} onChange={this.handleChange} />
+                    <FormHelperText id="email-address-input-field-helper-text">I'll never share your email with anyone so don't worry!</FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl required variant="filled" style={{ width: "75%" }}>
+                    <InputLabel htmlFor="message-input">Message</InputLabel>
+                    <Input id="message-input" name="message" required multiline rows="6" value={this.state.message} onChange={this.handleChange} />
+                </FormControl>
+            </Grid>
+            <Grid item style={{ paddingTop: "35px", paddingBottom: "35px" }}>
+                <Button size="large" variant="contained" color="primary" onClick={this.sendEmail}>SEND</Button>
+            </Grid>
+        </Grid>
     }
 }
